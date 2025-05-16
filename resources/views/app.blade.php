b2df3067116e380930b397afe0a2969fb235b661
DIEGO FERNANDO YAMA ANDRADE <diegoyamaa@gmail.com>
2025-05-16 11:08:06 -0500

    Refactor app.blade.php for improved structure; add CSRF token meta tag, adjust Vite configuration for local environment, and enhance script loading.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,28 +1,30 @@
-<?php
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
+    @routes
     @if (app()->environment('local'))
-        @vite(['resources/js/app.js'])
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @else
+        <!-- Usa los archivos compilados en producción -->
         <link rel="stylesheet" href="{{ asset('build/assets/app-CjICqXgl.css') }}">
-        <script type="module" src="{{ asset('build/assets/app-DpUerojF.js') }}" defer></script>
+        <script type="module" src="{{ asset('build/assets/app-DpUerojF.js') }}"></script>
     @endif
-    
     @inertiaHead
 </head>
+
 <body class="font-sans antialiased">
     @inertia
 </body>
+
 </html>
