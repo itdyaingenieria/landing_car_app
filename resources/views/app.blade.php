bbfecab7372fd3c7725c954dc4dea04599881294
DIEGO FERNANDO YAMA ANDRADE <diegoyamaa@gmail.com>
2025-05-16 11:06:01 -0500

    Refactor app.blade.php for improved structure and clarity; remove unnecessary routes directive and adjust Vite configuration for local environment.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,30 +1,28 @@
+<?php
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @routes
     @if (app()->environment('local'))
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.js'])
     @else
-        <!-- Usa los archivos compilados en producción -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-        <script type="module" src="{{ asset('build/assets/app.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('build/assets/app-CjICqXgl.css') }}">
+        <script type="module" src="{{ asset('build/assets/app-DpUerojF.js') }}" defer></script>
     @endif
+    
     @inertiaHead
 </head>
-
 <body class="font-sans antialiased">
     @inertia
 </body>
-
 </html>
